ef5b316b782ac0a9d875355059785aaf9463f5ec
mingmuchuan <1871998048@qq.com>
2021-01-22 09:50:04 +0800

    edit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <Layout>
+    <h1>lg-gridsome预渲染页面</h1>
     <ul>
       <!-- <li v-for="post in $page.posts.edges" :key="post.node.id" @click="handleRoute(post.node.id)">
         <div class="title">{{ post.node.title }}</div>
@@ -7,6 +8,26 @@
         <div class="date">{{ post.node.create_at }}</div>
       </li>   -->
       <li>
+        <div class="title">标题</div>
+        <div class="content">内容</div>
+        <div class="date">2020:21:21 23:32:32</div>
+      </li>  
+      <li>
+        <div class="title">标题</div>
+        <div class="content">内容</div>
+        <div class="date">2020:21:21 23:32:32</div>
+      </li>  
+        <li>
+        <div class="title">标题</div>
+        <div class="content">内容</div>
+        <div class="date">2020:21:21 23:32:32</div>
+      </li>  
+      <li>
+        <div class="title">标题</div>
+        <div class="content">内容</div>
+        <div class="date">2020:21:21 23:32:32</div>
+      </li> 
+          <li>
         <div class="title">标题</div>
         <div class="content">内容</div>
         <div class="date">2020:21:21 23:32:32</div>
